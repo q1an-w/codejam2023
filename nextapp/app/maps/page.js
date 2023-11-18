9b19dbfdb784d244391555d60097afb675459c0f
p_carlss <philipcarlssoncoulombe@gmail.com>
2023-11-18 15:23:53 -0500

    mobile format

--- nextapp/app/maps/page.js
+++ nextapp/app/maps/page.js
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 import React, { useEffect } from "react";
 
 export default function Maps() {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAKxCpR8daQzrrMy8a9Z4MKhrdZV2CsaUI`; // Replace YOUR_API_KEY with your Google Maps API key
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAKxCpR8daQzrrMy8a9Z4MKhrdZV2CsaUI`; // Replace with your Google Maps API key
     script.async = true;
     document.body.appendChild(script);
 
     script.onload = () => {
       const map = new window.google.maps.Map(document.getElementById("map"), {
         center: { lat: 45.5017, lng: -73.5673 }, // Montreal coordinates
-        zoom: 11,
+        zoom: 8,
       });
 
       const addMarker = (location) => {
@@ -40,18 +40,36 @@ export default function Maps() {
     };
   }, []);
 
+  const Palette = {
+    width: "100%",
+    margin: "auto",
+    color: "black",
+    fontFamily: "Arial, sans-serif",
+  };
+
   return (
-    <div>
-      <div
-        id="map"
-        style={{
-          marginTop: "50px",
-          margin: "auto",
-          width: "50%", // Adjusted width to fill the available space
-          height: "800px", // Reduced height for better visibility
-          border: "4px solid black", // Adjusted border size
-        }}
-      ></div>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={Palette}>
+        <h1
+          style={{
+            textAlign: "center",
+            fontSize: "40px",
+            marginBottom: "20px",
+          }}
+        >
+          Load Finder
+        </h1>
+
+        <div style={{ height: "400px", width: "95%", margin: "auto" }}>
+          <div
+            id="map"
+            style={{
+              height: "100%", // Adjusted height to fill the available space
+              border: "4px solid black", // Adjusted border size
+            }}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
