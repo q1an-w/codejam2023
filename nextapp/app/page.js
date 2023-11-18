2037e30d72804cda835e4b208971834f293820ee
p_carlss <philipcarlssoncoulombe@gmail.com>
2023-11-18 05:32:49 -0500

    update

--- nextapp/app/page.js
+++ nextapp/app/page.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <Link href="/truckList">
-        Display Test
+      <Link href="/home">
+        Home
       </Link>
     </main>
   );
