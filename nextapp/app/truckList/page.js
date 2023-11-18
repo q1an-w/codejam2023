4b4d340f233ed6cce0d507db8f4bd4218c537eae
p_carlss <philipcarlssoncoulombe@gmail.com>
2023-11-18 14:34:50 -0500

    working map with manual markers

--- nextapp/app/truckList/page.js
+++ nextapp/app/truckList/page.js
@@ -7,177 +7,132 @@ import {
   maxDistanceFilter,
 } from "./getList.js";
 
+
 export default function OtherPage() {
-  const yellowPalette = {
+  const Palette = {
     width: "80%",
     margin: "auto",
     color: "black",
     padding: "20px",
     fontFamily: "Arial, sans-serif",
-    margin: "0",
   };
 
   const tableStyle = {
     margin: "auto",
     width: "100%",
     borderCollapse: "collapse",
-    marginTop: "10px",
-    border: "2px solid #000", // Add border to the table
+    marginTop: "20px",
+    border: "2px solid #ddd",
   };
 
   const thStyle = {
-    margin: "auto",
-    paddingBottom: "10px",
-    fontSize: "25px",
+    padding: "12px",
+    fontSize: "20px",
     textAlign: "center",
-    width: "33%",
-    border: "1px solid #000", // Add borders to table headers
+    border: "2px solid #ddd",
   };
 
   const tdStyle = {
-    fontSize: "20px",
+    color: "black",
+    padding: "10px",
+    fontSize: "18px",
     textAlign: "center",
-    width: "33%",
-    border: "1px solid #000", // Add borders to table cells
+    border: "1px solid #ddd",
   };
 
-  // Define a mapping of colors to truck IDs
-  const colorMapping = {};
+  const colorList = [
+    "#FFB380", "#FFD9B3", "#FFB3FF", "#FFFFCC", "#B3E6FF",
+    "#FFD699", "#B3B3FF", "#CCCCB3", "#CCFFCC", "#FFCCCC",
+    "#CCFF99", "#CCFF66", "#FFCCCC", "#B3CCFF", "#CCFF99",
+    "#FFCCFF", "#E6FF99", "#FF99B3", "#FFB3B3", "#B3FFE6",
+    "#CCFFCC", "#E6B3FF", "#CCFF66", "#FFB380", "#E6B3E6",
+    "#B3B3CC", "#FFB3FF", "#FFCCFF", "#B3E6FF", "#B3FFCC"
+  ];  
+  
+  const getColorForTruckId = (truckId) => {
+    const randomIndex = Math.floor(Math.random() * colorList.length);
+    return colorList[randomIndex];
+  };
 
-  // Function to generate a random color
-  function getRandomColor() {
-    return "#" + Math.floor(Math.random() * 16777215).toString(16);
-  }
-
-  function getColorForTruckId(truckId) {
-    if (!colorMapping[truckId]) {
-      colorMapping[truckId] = getRandomColor(); // Generate a color for new truck IDs
-    }
-    return colorMapping[truckId];
-  }
+  const generateRowColor = (truckId) => {
+    const color = getColorForTruckId(truckId);
+    return { backgroundColor: color, color: "#fff" };
+  };
 
   return (
-    <main style={yellowPalette}>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div style={{ width: "48%" }}>
-          <h2 style={{ fontSize: "30px" }}>Truck Data</h2>
-          {/* Truck Data table */}
-          <table style={tableStyle}>
-            <thead>
-              <tr>
-                <th style={thStyle}>ID</th>
-                <th style={thStyle}>Latitude</th>
-                <th style={thStyle}>Longitude</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Tdata.map((data, index) => (
-                <tr key={`T${index}`}>
-                  <td style={tdStyle}>{data.id}</td>
-                  <td style={tdStyle}>{data.latitude}</td>
-                  <td style={tdStyle}>{data.longitude}</td>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={Palette}>
+        <h1 style={{ textAlign: "center", fontSize: "40px", marginBottom: "20px" }}>Admin Dashboard</h1>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ width: "48%" }}>
+            <h2 style={{ fontSize: "30px", marginBottom: "10px" }}>Truck Data</h2>
+            <table style={tableStyle}>
+              <thead>
+                <tr>
+                  <th style={thStyle}>ID</th>
+                  <th style={thStyle}>Latitude</th>
+                  <th style={thStyle}>Longitude</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div style={{ width: "48%" }}>
-          <h2 style={{ fontSize: "30px" }}>Load Data</h2>
-          {/* Load Data table */}
-          <table style={tableStyle}>
-            <thead>
-              <tr>
-                <th style={thStyle}>ID</th>
-                <th style={thStyle}>Latitude</th>
-                <th style={thStyle}>Longitude</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Ldata.map((data, index) => (
-                <tr key={`L${index}`}>
-                  <td style={tdStyle}>{data.id}</td>
-                  <td style={tdStyle}>{data.latitude}</td>
-                  <td style={tdStyle}>{data.longitude}</td>
+              </thead>
+              <tbody>
+                {Tdata.map((data, index) => (
+                  <tr key={`T${index}`}>
+                    <td style={tdStyle}>{data.id}</td>
+                    <td style={tdStyle}>{data.latitude}</td>
+                    <td style={tdStyle}>{data.longitude}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div style={{ width: "48%" }}>
+            <h2 style={{ fontSize: "30px", marginBottom: "10px" }}>Load Data</h2>
+            <table style={tableStyle}>
+              <thead>
+                <tr>
+                  <th style={thStyle}>ID</th>
+                  <th style={thStyle}>Latitude</th>
+                  <th style={thStyle}>Longitude</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {Ldata.map((data, index) => (
+                  <tr key={`L${index}`}>
+                    <td style={tdStyle}>{data.id}</td>
+                    <td style={tdStyle}>{data.latitude}</td>
+                    <td style={tdStyle}>{data.longitude}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
+
+        <h2 style={{ fontSize: "30px", marginBottom: "10px" }}>
+          Max Distance Filter Ascending Distances
+        </h2>
+        <table style={tableStyle}>
+          <thead>
+            <tr>
+              <th style={thStyle}>Truck ID</th>
+              <th style={thStyle}>Load ID</th>
+              <th style={thStyle}>Distance (miles)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {maxDistanceFilter.map((pair, index) => {
+              const rowColor = generateRowColor(pair.truck.id);
+              return (
+                <tr key={`D${index}`} style={rowColor}>
+                  <td style={tdStyle}>{pair.truck.id}</td>
+                  <td style={tdStyle}>{pair.load.id}</td>
+                  <td style={tdStyle}>{pair.distance}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
-
-      {/* <h2 style={{ fontSize: "30px" }}>Distances</h2>
-      <table style={tableStyle}>
-        <thead>
-          <tr>
-            <th style={thStyle}>Truck ID</th>
-            <th style={thStyle}>Load ID</th>
-            <th style={thStyle}>Distance (miles)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {distances.map((pair, index) => (
-            <tr key={`D${index}`}>
-              <td style={tdStyle}>{pair.truck.id}</td>
-              <td style={tdStyle}>{pair.load.id}</td>
-              <td style={tdStyle}>{pair.distance}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <h2 style={{ fontSize: "30px" }}>Ascending Distances</h2>
-      <table style={tableStyle}>
-        <thead>
-          <tr>
-            <th style={thStyle}>Truck ID</th>
-            <th style={thStyle}>Load ID</th>
-            <th style={thStyle}>Distance (miles)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {ascendingDistances.map((pair, index) => (
-            <tr key={`D${index}`}>
-              <td style={tdStyle}>{pair.truck.id}</td>
-              <td style={tdStyle}>{pair.load.id}</td>
-              <td style={tdStyle}>{pair.distance}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table> */}
-
-      <h2 style={{ fontSize: "30px" }}>
-        Max Distance Filter Ascending Distances
-      </h2>
-      <table style={tableStyle}>
-        <thead>
-          <tr>
-            <th style={thStyle}>Truck ID</th>
-            <th style={thStyle}>Load ID</th>
-            <th style={thStyle}>Distance (miles)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {maxDistanceFilter.map((pair, index) => {
-            const rowColor = getColorForTruckId(pair.truck.id); // Get color for the current truck ID
-            return (
-              <tr key={`D${index}`}>
-                <td style={{ ...tdStyle, color: rowColor }}>
-                  {pair.truck.id}
-                </td>
-                <td style={{ ...tdStyle, color: rowColor }}>
-                  {pair.load.id}
-                </td>
-                <td style={{ ...tdStyle, color: rowColor }}>
-                  {pair.distance}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-
-      
-      
-    </main>
+    </div>
   );
 }
