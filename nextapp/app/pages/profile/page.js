15879b3758c01b97ad387d802762b18c7d806575
ym-liu <yu.meng.liu@mail.mcgill.ca>
2023-11-19 02:28:16 -0500

    more profile stuff

--- nextapp/app/pages/profile/page.js
+++ nextapp/app/pages/profile/page.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "../../main.css";
+import "./style.css";
 import whitelogo from "../../imgs/logo white background.png";
 import Image from "next/image";
 import pfp from "./mikesanders.png";
 
 export default function Profile() {
   const name = "Mike Sanders"; // change to fetch user full name
+  const age = 41; // change to fetch user age
+  const phone = "(514) 866-6686"; // change to fetch user phone#
+  const license = "954-TMD";
+  const maxload = 36287;
   return (
     <main id="2p">
       <link
@@ -16,6 +21,31 @@ export default function Profile() {
       <div className="profile-div">
         <div className="profile-name">{name}</div>
         <Image src={pfp} className="pfp" alt="image" />
+      </div>
+
+      <div class="profile-box">
+        <div className="profile-heading">Name</div>
+        <div className="input-group" id="input-group-section">
+          <input type="text" id="text-input" placeholder={name} required />
+        </div>
+        <div className="input-group" id="input-group-section">
+          <input type="text" id="text-input" placeholder={age} required />
+        </div>
+        <div className="input-group" id="input-group-section">
+          <input type="text" id="text-input" placeholder={phone} required />
+        </div>
+      </div>
+
+      <div className="input-group" id="input-group-section">
+        <input type="text" id="text-input" placeholder={license} required />
+      </div>
+      <div className="input-group" id="input-group-section">
+        <input
+          type="text"
+          id="text-input"
+          placeholder={maxload + " kg"}
+          required
+        />
       </div>
 
       <div className="navbar">
